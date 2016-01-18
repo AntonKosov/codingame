3acbb16e822bd90ff0e74e092eaa425b2846bbfa
kosovar <kosov.anton.rodionovich@gmail.com>
2016-01-18 20:02:46 +0300

    refactoring

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -6,13 +6,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class PlayerTest {
+    
     @Test
     public void simplest() {
-        String data = "3\n" +
-                "3\n" +
+        startTest(3, 3,
                 "1.2\n" +
                 "...\n" +
-                "..1\n";
+                "..1\n",
+                "0 0 2 0 1\n" +
+                "2 0 2 2 1\n");
+    }
+
+    private void startTest(int width, int height, String in, String answer) {
+        String data = width + "\n" + height + "\n" + in;
         System.setIn(new ByteArrayInputStream(data.getBytes()));
 
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -22,30 +28,8 @@ public class PlayerTest {
         Player.main(null);
 
         System.out.flush();
-        String answer = stream.toString();
+        String out = stream.toString();
 
-        Assert.assertEquals(
-                "0 0 2 0 1\n" +
-                "2 0 2 2 1\n", answer);
+        Assert.assertEquals(answer, out);
     }
-
-//    @Test
-//    public void simpleTest() {
-//        String data = "2\n" +
-//                "2\n" +
-//                "42\n" +
-//                "31\n";
-//        System.setIn(new ByteArrayInputStream(data.getBytes()));
-//
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(stream);
-//        System.setOut(printStream);
-//
-//        Player.main(null);
-//
-//        System.out.flush();
-//        String answer = stream.toString();
-//
-//        Assert.assertEquals("", answer);
-//    }
 }
