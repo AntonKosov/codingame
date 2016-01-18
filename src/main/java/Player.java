c828ab7c70fc860c8a4c5f30d1c1b08f8a0a53c3
kosovar <kosov.anton.rodionovich@gmail.com>
2016-01-18 08:57:27 +0300

    unit tests were added

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,6 +1,4 @@
 import java.util.*;
-import java.io.*;
-import java.math.*;
 
 /**
  * The machines are gaining ground. Time to show them what we're really made of...
@@ -9,11 +7,15 @@ class Player {
     
     private static int width;
     private static int height;
+    private static int countNodes;
+
+    private static Matrix matrix;
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         width = in.nextInt(); // the number of cells on the X axis
         height = in.nextInt(); // the number of cells on the Y axis
+        countNodes = width * height;
         String[] lines = new String[height];
         in.nextLine();
         for (int i = 0; i < height; i++) {
@@ -21,17 +23,41 @@ class Player {
             lines[i] = line;
         }
         
-        Matrix m = new Matrix(lines);
+        matrix = new Matrix(lines);
+        lookingExactSolutions();
 
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("0 0 2 0 1"); // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+        String[] answers = new String[4];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                answers[0] = matrix.getNeighbor(x, y, x, y - 1);
+                answers[1] = matrix.getNeighbor(x, y, x + 1, y);
+                answers[2] = matrix.getNeighbor(x, y, x, y + 1);
+                answers[3] = matrix.getNeighbor(x, y, x - 1, y);
+                for (String answer : answers) {
+                    if (answer != null) {
+                        System.out.println(answer); // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+                    }
+                }
+            }
+        }
+    }
+
+    private static void lookingExactSolutions() {
+        boolean isRepeat = true;
+        while (isRepeat) {
+            isRepeat = false;
+            for (int i = 0; i < countNodes; i++) {
+                isRepeat |= matrix.distributeIsPossible(i);
+            }
+        }
     }
     
     private static class Matrix {
         
-        private final Integer[][] data = new Integer[width * height][width * height];
+        private final Integer[][] data = new Integer[countNodes][countNodes];
         
         private final int[] freeInRows = new int[height];
         private final int[] freeInColumns = new int[width];
@@ -45,19 +71,19 @@ class Player {
                         continue;
                     }
                     int nodeIndex = cn(c, l);
-                    int val = Integer.parseInt(ch);
+                    int val = Character.getNumericValue(ch);
                     data[nodeIndex][nodeIndex] = val;
                 }
             }
 
-            for (int y = 0; y < height; y+) {
+            for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     if (y > 0 && data[cn(x, y - 1)][cn(x, y - 1)] != null) {
                         data[cn(x, y)][cn(x, y - 1)] = 0;
                         freeInColumns[cn(x, y)]++;
                         freeInRows[cn(x, y - 1)]++;
                     }
-                    if (x < width - 1 && data[cn(x + 1, y)][x + 1, y] != null) {
+                    if (x < width - 1 && data[cn(x + 1, y)][cn(x + 1, y)] != null) {
                         data[cn(x, y)][cn(x + 1, y)] = 0;
                         freeInColumns[cn(x, y)]++;
                         freeInRows[cn(x + 1, y)]++;
@@ -78,6 +104,45 @@ class Player {
         
         private static int cn(int x, int y) {
             return y * height + x;
+        }
+
+        public String getNeighbor(int sX, int sY, int tX, int tY) {
+            if (tX < 0 || tX > width - 1 || tY < 0 || tY > height - 1) {
+                return null;
+            }
+
+            Integer value = data[cn(sX, sY)][cn(tX, tY)];
+
+            if (value == null || value == 0) {
+                return null;
+            }
+
+            return sX + " " + sY + " " + tX + " " + tY + " 1";
+        }
+
+        public boolean distributeIsPossible(int node) {
+            if (data[node][node] != freeInColumns[node] + freeInRows[node]) {
+                return false;
+            }
+
+            for (int i = 0; i < width; i++) {
+                if (data[i][node] != null && data[i][node] == 0) {
+                    data[i][node] = 1;
+                }
+            }
+
+            for (int i = 0; i < height; i++) {
+                if (data[node][i] != null && data[node][i] == 0) {
+                    data[node][i] = 1;
+                }
+            }
+
+            data[node][node] = 0;
+
+            freeInColumns[node] = 0;
+            freeInRows[node] = 0;
+
+            return true;
         }
     }
 }
