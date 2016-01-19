f10ee3b0bce058f891d1e2bdccb69c0f0230f907
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2016-01-19 20:36:24 +0300

    stack was added

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -11,6 +11,8 @@ class Player {
 
     private static Matrix matrix;
 
+    private static final Stack<State> stack = new Stack<State>();
+
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         width = in.nextInt(); // the number of cells on the X axis
@@ -26,23 +28,34 @@ class Player {
         }
         
         matrix = new Matrix(lines);
-        lookingExactSolutions();
+        lookingSolutions();
+        if (matrix.isSolved()) {
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
 
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-        ArrayList<String> answers = matrix.getLinks();
-        for (String answer : answers) {
-            System.out.println(answer); // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+            ArrayList<String> answers = matrix.getLinks();
+            for (String answer : answers) {
+                System.out.println(answer); // Two coordinates and one integer: a node, one of its neighbors, the number of links connecting them.
+            }
+        } else {
+            System.out.println("No solution");
         }
     }
 
-    private static void lookingExactSolutions() {
+    private static void lookingSolutions() {
+        final int startStakSize = stack.size();
+
         boolean isRepeat = true;
         while (isRepeat) {
             isRepeat = false;
             for (int i = 0; i < countNodes; i++) {
                 isRepeat |= matrix.distributeIsPossible(i);
+            }
+        }
+
+        if (!matrix.isSolved()) {
+            while (stack.size() > startStakSize) {
+                matrix.restore(stack.pop());
             }
         }
     }
@@ -53,6 +66,8 @@ class Player {
         
         private final int[] freeInRows = new int[countNodes];
         private final int[] freeInColumns = new int[countNodes];
+
+        private int countFullNodes = 0;
         
         public Matrix(String[] lines) {
             boolean[][] field = new boolean[width][height];
@@ -68,6 +83,7 @@ class Player {
                     int val = Character.getNumericValue(ch);
                     data[nodeIndex][nodeIndex] = val;
                     field[c][l] = true;
+                    countFullNodes++;
                 }
             }
 
@@ -93,6 +109,10 @@ class Player {
                     }
                 }
             }
+        }
+
+        public boolean isSolved() {
+            return countFullNodes == 0;
         }
         
         private static int cn(int x, int y) {
@@ -154,11 +174,20 @@ class Player {
                 }
             }
 
+            final int startStackSize = stack.size();
+
             for (int i = 0; i < countNodes; i++) {
                 if (i != node) {
                     Integer weight = getWeight(i, node);
                     if (weight != null && weight < 2) {
-                        distribute(i, node, Math.min(data[node][node], 2 - weight));
+                        final State state = distribute(i, node, Math.min(data[node][node], 2 - weight));
+                        if (state.isNoSolution) {
+                            restore(state);
+                            while (stack.size() > startStackSize) {
+                                restore(stack.pop());
+                            }
+                        }
+                        stack.push(state);
                         if (data[node][node] == 0) {
                             break;
                         }
@@ -166,15 +195,38 @@ class Player {
                 }
             }
 
-            return true;
+            return stack.size() > startStackSize;
         }
 
-        private void distribute(int tX, int tY, int weight) {
-            setWeight(tX, tY, getWeight(tX, tY) + weight);
-            freeInRows[tY] -= weight;
-            freeInColumns[tX] -= weight;
-            data[tX][tX] -= weight;
-            data[tY][tY] -=weight;
+        private State distribute(int x, int y, int value) {
+            setWeight(x, y, getWeight(x, y) + value);
+            freeInRows[y] -= value;
+            freeInColumns[x] -= value;
+            data[x][x] -= value;
+            data[y][y] -= value;
+            if (data[x][x] == 0) {
+                countFullNodes--;
+            }
+            if (data[y][y] == 0) {
+                countFullNodes--;
+            }
+
+            boolean isNoSolution = data[x][x] > freeInColumns[x] + freeInRows[x] || data[y][y] > freeInColumns[y] + freeInRows[y];
+            return new State(x, y, value, isNoSolution);
+        }
+
+        private void restore(State state) {
+            setWeight(state.x, state.y, getWeight(state.x, state.y) - state.value);
+            freeInRows[state.y] += state.value;
+            freeInColumns[state.x] += state.value;
+            if (data[state.x][state.x] == 0) {
+                countFullNodes++;
+            }
+            if (data[state.y][state.y] == 0) {
+                countFullNodes++;
+            }
+            data[state.x][state.x] += state.value;
+            data[state.y][state.y] += state.value;
         }
 
         private void setWeight(int node1, int node2, int weight) {
@@ -183,6 +235,21 @@ class Player {
             } else {
                 data[node2][node1] = weight;
             }
+        }
+    }
+
+    private static class State {
+
+        public final int x;
+        public final int y;
+        public final int value;
+        public final boolean isNoSolution;
+
+        public State(int x, int y, int value, boolean isNoSolution) {
+            this.x = x;
+            this.y = y;
+            this.value = value;
+            this.isNoSolution = isNoSolution;
         }
     }
 }
