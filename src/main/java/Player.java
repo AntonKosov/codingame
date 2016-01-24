8ae20b30e21af2e1bffab1575c81f754db7e6af2
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2016-01-24 17:52:55 +0300

    bug fix

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,7 +4,12 @@ import java.util.*;
  * The machines are gaining ground. Time to show them what we're really made of...
  **/
 class Player {
-    
+
+    public static final int UP = 0;
+    public static final int DOWN = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
+
     private static int width;
     private static int height;
     private static int countActiveNodes;
@@ -87,7 +92,7 @@ class Player {
             final Link[] links = node.links;
             for (int i = startPoint; i < links.length; i++) {
                 final Link link = links[i];
-                if (link != null && link.countFreeLinks() > 0 && link.node1.value > 0 && link.node2.value > 0) {
+                if (link != null && link.countFreeLinks() > 0 && !link.isBlocked() && link.node1.value > 0 && link.node2.value > 0) {
                     final State state = map.distribute(link, 1);
                     if (state.isNoSolution) {
                         map.restore(state);
@@ -133,25 +138,68 @@ class Player {
                     for (int i = x - 1; i >= 0; i--) {
                         final Node sibling = nodes[i][y];
                         if (sibling != null) {
-                            node.siblings[Node.LEFT] = sibling;
-                            sibling.siblings[Node.RIGHT] = node;
+                            node.siblings[LEFT] = sibling;
+                            sibling.siblings[RIGHT] = node;
                             int possibleLink = Math.min(2, Math.min(node.value, sibling.value));
                             final Link link = new Link(possibleLink, node, sibling);
-                            node.links[Node.LEFT] = link;
-                            sibling.links[Node.RIGHT] = link;
+                            node.links[LEFT] = link;
+                            sibling.links[RIGHT] = link;
                             break;
                         }
                     }
                     for (int i = y - 1; i >= 0; i--) {
                         final Node sibling = nodes[x][i];
                         if (sibling != null) {
-                            node.siblings[Node.UP] = sibling;
-                            sibling.siblings[Node.DOWN] = node;
+                            node.siblings[UP] = sibling;
+                            sibling.siblings[DOWN] = node;
                             int possibleLink = Math.min(2, Math.min(node.value, sibling.value));
                             final Link link = new Link(possibleLink, node, sibling);
-                            node.links[Node.UP] = link;
-                            sibling.links[Node.DOWN] = link;
+                            node.links[UP] = link;
+                            sibling.links[DOWN] = link;
                             break;
+                        }
+                    }
+                }
+            }
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    Node node = nodes[x][y];
+                    if (node != null) {
+                        Link upLink = node.links[UP];
+                        if (upLink != null) {
+                            int yStart = Math.min(upLink.node1.y, upLink.node2.y) + 1;
+                            int yEnd = Math.max(upLink.node1.y, upLink.node2.y) - 1;
+                            for (int ys = yStart; ys <= yEnd; ys++) {
+                                for (int xs = upLink.node1.x - 1; xs >= 0; xs--) {
+                                    Node sn = nodes[xs][ys];
+                                    if (sn != null) {
+                                        final Link crossLink = sn.links[RIGHT];
+                                        if (crossLink != null) {
+                                            upLink.crosses.add(crossLink);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        Link leftLink = node.links[LEFT];
+                        if (leftLink != null) {
+                            int xStart = Math.min(leftLink.node1.x, leftLink.node2.x) + 1;
+                            int xEnd = Math.max(leftLink.node1.x, leftLink.node2.x) - 1;
+                            for (int xs = xStart; xs <= xEnd; xs++) {
+                                for (int ys = leftLink.node1.y - 1; ys >= 0; ys--) {
+                                    Node sn = nodes[xs][ys];
+                                    if (sn != null) {
+                                        final Link crossLink = sn.links[DOWN];
+                                        if (crossLink != null) {
+                                            leftLink.crosses.add(crossLink);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -162,7 +210,7 @@ class Player {
 
         public boolean isSolved() {
             if (countNotEmptyNodes > 0) return false;
-            final int c = countLinks();
+            final int c = countActiveLinks();
             if (c < countActiveNodes - 1) {
                 System.err.println("isSolved: " + c + "/" + countActiveNodes);
                 return false;
@@ -179,13 +227,13 @@ class Player {
                 for (int x = 0; x < width; x++) {
                     Node node = nodes[x][y];
                     if (node != null) {
-                        final Link upLink = node.links[Node.UP];
-                        if (upLink != null && upLink.value > 0) {
+                        final Link upLink = node.links[UP];
+                        if (Link.isActive(upLink)) {
                             result.add(upLink.node2.x + " " + upLink.node2.y +" " +
                                     upLink.node1.x + " " + upLink.node1.y + " " + upLink.value);
                         }
-                        final Link leftLink = node.links[Node.LEFT];
-                        if (leftLink != null && leftLink.value > 0) {
+                        final Link leftLink = node.links[LEFT];
+                        if (Link.isActive(leftLink)) {
                             result.add(leftLink.node2.x + " " + leftLink.node2.y +" " +
                                     leftLink.node1.x + " " + leftLink.node1.y + " " + leftLink.value);
                         }
@@ -197,17 +245,18 @@ class Player {
         }
 
         //todo optimization
-        private int countLinks() {
+        private int countActiveLinks() {
             int result = 0;
 
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     final Node node = nodes[x][y];
                     if (node != null) {
-                        for (Link link : node.links) {
-                            if (link != null && link.value > 0) {
-                                result++;
-                            }
+                        if (Link.isActive(node.links[UP])) {
+                            result++;
+                        }
+                        if (Link.isActive(node.links[LEFT])) {
+                            result++;
                         }
                     }
                 }
@@ -231,7 +280,7 @@ class Player {
             for (Link link : node.links) {
                 if (link != null) {
                     final int countFreeLinks = link.countFreeLinks();
-                    if (countFreeLinks > 0) {
+                    if (countFreeLinks > 0 && link.node1.value > 0 && link.node2.value > 0) {
                         int countLinks = Math.min(countFreeLinks, link.node1.value);
                         countLinks = Math.min(countLinks, link.node2.value);
                         final State state = distribute(link, countLinks);
@@ -253,7 +302,8 @@ class Player {
         public State distribute(Link link, int value) {
             step++;
             final int newValue = link.value + value;
-            System.err.println("step=" + step + ", before:\n" + toString());
+            System.err.println(">>>before: step=" + step + ", l=" + link.node2 + "-" + link.node1 +
+                    ", v=" + value + "\n" + toString());
             link.value = newValue;
             link.node1.value -= value;
             link.node2.value -= value;
@@ -268,23 +318,33 @@ class Player {
                 countDoubleLinks++;
             }
 
+            for (Link lc : link.crosses) {
+                lc.block();
+            }
+
             boolean isNoSolution =
                     countDoubleLinks > maxDoubleLinks ||
-//                    link.node1.value < 0 ||
-//                    link.node2.value < 0 ||
-                    link.node1.isNotPossible(true) ||
-                    link.node2.isNotPossible(true);
+                    !link.node1.isPossible(true) ||
+                    !link.node2.isPossible(true);
+            if (!isNoSolution) {
+                for (Link lc : link.crosses) {
+                    isNoSolution = !lc.node1.isPossible(false) || !lc.node2.isPossible(false);
+                    if (isNoSolution) {
+                        break;
+                    }
+                }
+            }
 
             if (!isNoSolution) {
                 System.err.println(
                         "\nstep=" + step +
                         ", stack=" + stack.size());
                 System.err.println(toString());
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    Thread.sleep(50);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
             }
 
             return new State(link, value, isNoSolution);
@@ -304,15 +364,31 @@ class Player {
             }
             state.link.node1.value += state.value;
             state.link.node2.value += state.value;
+            for (Link lc : state.link.crosses) {
+                lc.unblock();
+            }
+            System.err.println("restore " + state);
         }
 
         @Override
         public String toString() {
             StringBuilder result = new StringBuilder();
+            Link[] vLinks = new Link[width];
             for (int y = 0; y < height; y++) {
+                Link hLink = null;
                 for (int x = 0; x < width; x++) {
                     Node node = nodes[x][y];
-                    result.append(node == null ? "." : node.value);
+                    if (node != null) {
+                        hLink = node.links[RIGHT];
+                        vLinks[x] = node.links[DOWN];
+                        result.append(node.value);
+                    } else if (hLink != null && hLink.value > 0) {
+                        result.append("-");
+                    } else if (vLinks[x] != null && vLinks[x].value > 0) {
+                        result.append("|");
+                    } else {
+                        result.append(".");
+                    }
                 }
                 result.append("\n");
             }
@@ -332,15 +408,15 @@ class Player {
             this.value = value;
             this.isNoSolution = isNoSolution;
         }
+
+        @Override
+        public String toString() {
+            return "l=" + link + ", v=" + value;
+        }
     }
 
 
     private static class Node {
-        public static final int UP = 0;
-        public static final int DOWN = 1;
-        public static final int LEFT = 2;
-        public static final int RIGHT = 3;
-
         public final Node[] siblings = new Node[4];
         public final Link[] links = new Link[4];
 
@@ -360,7 +436,7 @@ class Player {
 
             for (Link link : links) {
                 if (link != null) {
-                    result += link.maxValue - link.value;
+                    result += Math.min(link.node2.value, Math.min(link.node1.value, link.maxValue - link.value));
                 }
             }
 
@@ -379,14 +455,14 @@ class Player {
             return result == 1;
         }
 
-        public boolean isNotPossible(boolean checkSiblings) {
+        public boolean isPossible(boolean checkSiblings) {
             if (value > countRestLinks()) {
                 return false;
             }
 
             if (checkSiblings) {
                 for (Node node : siblings) {
-                    if (node != null && node.isNotPossible(false)) {
+                    if (node != null && !node.isPossible(false)) {
                         return false;
                     }
                 }
@@ -395,6 +471,10 @@ class Player {
             return true;
         }
 
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
     }
 
     private static class Link {
@@ -403,6 +483,10 @@ class Player {
 
         public final Node node1;
         public final Node node2;
+
+        public final ArrayList<Link> crosses = new ArrayList<Link>();
+
+        public int blockCounter = 0;
 
         public Link(int maxValue, Node node1, Node node2) {
             this.maxValue = maxValue;
@@ -416,7 +500,27 @@ class Player {
 
         @Override
         public String toString() {
-            return value + "/" + maxValue;
+            return value + "/" + maxValue + " " + (isBlocked() ? "b" : "u") + ", " + node2 + "-" + node1;
+        }
+
+        public void block() {
+            blockCounter++;
+            value = maxValue;
+        }
+
+        public void unblock() {
+            blockCounter--;
+            if (!isBlocked()) {
+                value = 0;
+            }
+        }
+
+        public boolean isBlocked() {
+            return blockCounter > 0;
+        }
+
+        public static boolean isActive(Link link) {
+            return link != null && link.value > 0 && !link.isBlocked();
         }
     }
 }
