6c527d196cee07f686affa17f363a97a93cb55ce
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2016-01-30 12:40:54 +0300

    the first maze is solved

---
+++ src/main/java/Player.java
@@ -0,0 +1,304 @@
+import java.util.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    private static final boolean SHOW_LOGS = true;
+    private static final boolean SLOW_MODE = false;
+
+    private static final int COUNT_PATHS = 3;
+    private static final int BEST_EXPLORE_PATH = 0;
+    private static final int BACK_PATH = 1;
+    private static final int TO_CONTROL_PATH = 2;
+
+    private static final int UP = 0;
+    private static final int RIGHT = 1;
+    private static final int DOWN = 2;
+    private static final int LEFT = 3;
+
+    private static final LinkedList<Cell> sTempQueue = new LinkedList<Cell>();
+
+    private static int sHeight;
+    private static int sWidth;
+
+    private static int sKX;
+    private static int sKY;
+
+    private static Cell[][] sMaze;
+
+    private static Cell sTeleport = null;
+
+    private static Cell sControl = null;
+
+    private static State sState = State.lookingControl;
+
+    public static void main(String args[]) {
+        final Scanner in = new Scanner(System.in);
+
+        sHeight = in.nextInt(); // number of rows.
+        sWidth = in.nextInt(); // number of columns.
+        final int timeOfAlarm = in.nextInt(); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+        log("h=" + sHeight + ", w=" + sWidth + ", a=" + timeOfAlarm);
+
+        sMaze = new Cell[sWidth][sHeight];
+        for (int y = 0; y < sHeight; y++) {
+            final int countRows = y == 0 || y == sHeight - 1 ? 3 : y == 1 || y == sHeight - 2 ? 4 : 5;
+            for (int x = 0; x < sWidth; x++) {
+                int countColumns = x == 0 || x == sWidth - 1 ? 3 : x == 1 || x == sWidth - 2 ? 4 : 5;
+                final Cell cell = new Cell(x, y);
+                cell.countUnknownCells = countColumns * countRows;
+                sMaze[x][y] = cell;
+            }
+        }
+
+        // game loop
+        final LinkedList<Cell> path = new LinkedList<Cell>();
+        while (true) {
+            sKY = in.nextInt(); // row where Kirk is located.
+            sKX = in.nextInt(); // column where Kirk is located.
+            log("kx=" + sKX + ", ky=" + sKY);
+            if (readMaze(in)) {
+                if (sState == State.lookingControl && sControl != null) {
+                    sState = State.lookingBackPath;
+                    log("The control is found");
+                }
+
+                if (sState == State.lookingControl) {
+                    lookForPaths(sKX, sKY, BEST_EXPLORE_PATH);
+                    calculateBestExplorePath(BEST_EXPLORE_PATH, path);
+                    log("Looking for the control");
+                } else if (sState == State.lookingBackPath) {
+                    lookForPaths(sControl.x, sControl.y, BACK_PATH);
+                    if (sTeleport.paths[BACK_PATH] <= timeOfAlarm) {
+                        sState = State.goToControl;
+                        lookForPaths(sKX, sKY, TO_CONTROL_PATH);
+                        calculatePath(TO_CONTROL_PATH, sControl, path);
+                        log("Go to the control");
+                    } else {
+                        lookForPaths(sKX, sKY, BEST_EXPLORE_PATH);
+                        calculateBestExplorePath(BEST_EXPLORE_PATH, path);
+                        log("Looking for a better path to control");
+                    }
+                } else if (sState == State.goToControl) {
+                    //nothing
+                } else if (sState == State.goToTeleport) {
+                    //nothing
+                }
+            }
+
+            final Cell nextCell = path.pop();
+            if (nextCell == sControl) {
+                sState = State.goToTeleport;
+                calculatePath(BACK_PATH, sTeleport, path);
+                log("Go to the teleport");
+            }
+            String nextStep;
+            if (nextCell.y < sKY) {
+                nextStep = "UP";
+            } else if (nextCell.x > sKX) {
+                nextStep = "RIGHT";
+            } else if (nextCell.y > sKY) {
+                nextStep = "DOWN";
+            } else {
+                nextStep = "LEFT";
+            }
+
+            System.out.println(nextStep); // Kirk's next move (UP DOWN LEFT or RIGHT).
+            if (SLOW_MODE) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static void calculatePath(int indexOfPath, Cell finishCell, LinkedList<Cell> result) {
+        result.clear();
+        Cell currentCell = finishCell;
+        while (currentCell != null) {
+            result.add(0, currentCell);
+            Cell nextCell = null;
+            for (Cell neighbour : currentCell.neighbours) {
+                if (neighbour != null && neighbour.paths[indexOfPath] + 1 == currentCell.paths[indexOfPath]) {
+                    if (nextCell == null || nextCell.indexOfExplore < neighbour.indexOfExplore) {
+                        nextCell = neighbour;
+                    }
+                }
+            }
+            currentCell = nextCell;
+        }
+        result.pop(); // current point
+    }
+
+    private static void calculateBestExplorePath(int indexOfPath, LinkedList<Cell> result) {
+        sTempQueue.clear();
+        final Cell firstCell = sMaze[sKX][sKY];
+        firstCell.indexOfExplore = 0;
+        sTempQueue.push(firstCell);
+        Cell bestCell = null;
+        while (!sTempQueue.isEmpty()) {
+            final Cell currentCell = sTempQueue.pop();
+            final int nextStep = currentCell.paths[indexOfPath] + 1;
+            for (Cell neighbour : currentCell.neighbours) {
+                if (neighbour != null && neighbour.paths[indexOfPath] == nextStep) {
+                    neighbour.indexOfExplore = currentCell.indexOfExplore + ((float) neighbour.countUnknownCells) / nextStep / nextStep;
+                    if (bestCell == null || bestCell.indexOfExplore < neighbour.indexOfExplore) {
+                        bestCell = neighbour;
+                    }
+                    sTempQueue.push(neighbour);
+                }
+            }
+        }
+
+        calculatePath(indexOfPath, bestCell, result);
+    }
+
+    private static void lookForPaths(int sX, int sY, int indexOfPath) {
+        for (int x = 0; x < sWidth; x++) {
+            for (int y = 0; y < sHeight; y++) {
+                sMaze[x][y].paths[indexOfPath] = Integer.MAX_VALUE;
+            }
+        }
+        sMaze[sX][sY].paths[indexOfPath] = 0;
+
+        sTempQueue.clear();
+        sTempQueue.push(sMaze[sX][sY]);
+        while (!sTempQueue.isEmpty()) {
+            final Cell cell = sTempQueue.pop();
+            final int nextStep = cell.paths[indexOfPath] + 1;
+            for (Cell neighbour : cell.neighbours) {
+                if (neighbour != null && neighbour.paths[indexOfPath] > nextStep) {
+                    if (neighbour.type == CellType.control &&
+                            (sState == State.lookingControl || sState == State.lookingBackPath)) {
+                        continue;
+                    }
+                    neighbour.paths[indexOfPath] = nextStep;
+                    sTempQueue.push(neighbour);
+                }
+            }
+        }
+    }
+
+    private static boolean readMaze(Scanner in) {
+        boolean existsNewCells = false;
+        for (int y = 0; y < sHeight; y++) {
+            String row = in.next(); // C of the characters in '#.TC?' (i.e. one line of the ASCII maze).
+            if (SHOW_LOGS) {
+                if (y == sKY) {
+                    String rowForLog = "";
+                    if (sKX > 0) {
+                        rowForLog = row.substring(0, sKX);
+                    }
+                    rowForLog += "X";
+                    if (sKX < sWidth - 1) {
+                        rowForLog += row.substring(sKX + 1, sWidth);
+                    }
+                    log(rowForLog);
+                } else {
+                    log(row);
+                }
+            }
+            for (int x = 0; x < sWidth; x++) {
+                final char ch = row.charAt(x);
+                final Cell cell = sMaze[x][y];
+                if (cell.type != CellType.unknown) {
+                    continue;
+                }
+                existsNewCells = true;
+                switch (ch) {
+                    case '?':
+                        continue;
+                    case '#':
+                        openCell(cell, CellType.wall);
+                        break;
+                    case '.':
+                        openCell(cell, CellType.empty);
+                        break;
+                    case 'T':
+                        openCell(cell, CellType.teleport);
+                        sTeleport = cell;
+                        break;
+                    case 'C':
+                        openCell(cell, CellType.control);
+                        sControl = cell;
+                        break;
+                }
+
+            }
+        }
+
+        log("---");
+
+        return existsNewCells;
+    }
+
+    private static void openCell(Cell cell, CellType type) {
+        if (cell.type != CellType.unknown) {
+            return;
+        }
+
+        cell.type = type;
+        final int xs = cell.x > 1 ? cell.x - 2 : 0;
+        final int xe = cell.x < sWidth - 2 ? cell.x + 2 : sWidth - 1;
+        final int ys = cell.y > 1 ? cell.y - 2 : 0;
+        final int ye = cell.y < sHeight - 2 ? cell.y + 2 : sHeight - 1;
+        for (int x = xs; x <= xe; x++) {
+            for (int y = ys; y <= ye; y++) {
+                sMaze[x][y].countUnknownCells--;
+            }
+        }
+        if (type != CellType.wall) {
+            if (cell.x > 0) {
+                sMaze[cell.x - 1][cell.y].neighbours[RIGHT] = cell;
+            }
+            if (cell.x < sWidth - 1) {
+                sMaze[cell.x + 1][cell.y].neighbours[LEFT] = cell;
+            }
+            if (cell.y > 0) {
+                sMaze[cell.x][cell.y - 1].neighbours[DOWN] = cell;
+            }
+            if (cell.y < sHeight - 1) {
+                sMaze[cell.x][cell.y + 1].neighbours[UP] = cell;
+            }
+        }
+    }
+
+    private static void log(String message) {
+        if (SHOW_LOGS) {
+            System.err.println(message);
+        }
+    }
+
+    private enum CellType {
+        unknown, wall, empty, teleport, control
+    }
+
+    private enum State {
+        lookingControl, lookingBackPath, goToControl, goToTeleport
+    }
+
+    private static class Cell {
+        public final int x;
+        public final int y;
+        public int countUnknownCells = 0;
+        public CellType type = CellType.unknown;
+        public final int[] paths = new int[COUNT_PATHS];
+        public final Cell[] neighbours = new Cell[4];
+        public float indexOfExplore;
+
+        public Cell(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "x=" + x + ", y=" + y + ", t=" + type;
+        }
+    }
+}
---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,192 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Scanner.class, Player.class})
+public class PlayerTest {
+
+    private final LinkedList<String> mOutQueue = new LinkedList<String>();
+
+    private int mKX;
+    private int mKY;
+    private int mTurnsLeft;
+    private int mTurnsBackLeft;
+
+    private State mState;
+
+    private PrintStream mOriginalOutPrintStream;
+
+    @Before
+    public void before() {
+        mOriginalOutPrintStream = System.out;
+    }
+
+    public void after() {
+        System.setOut(mOriginalOutPrintStream);
+    }
+
+    @Test(expected = SuccessException.class)
+    public void easy1Direction() throws Exception {
+        startTest(7,
+                new String[]{
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "???############???????????????",
+                        "???############???????????????",
+                        "???##T......C##???????????????",
+                        "???############???????????????",
+                        "???############???????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????",
+                        "??????????????????????????????"
+                });
+    }
+
+    private void startTest(int alarm, final String[] maze) throws Exception {
+        final int height = maze.length;
+        final int width = maze[0].length();
+        mOutQueue.clear();
+        for (int y = 0; y < maze.length; y++) {
+            String row = maze[y];
+            final int indexOf = row.indexOf("T");
+            if (indexOf >= 0) {
+                mKX = indexOf;
+                mKY = y;
+                break;
+            }
+        }
+        mTurnsLeft = 1200;
+        mTurnsBackLeft = alarm;
+        mState = State.goToControl;
+
+        final boolean[][] visibility = new boolean[width][height];
+        setVisibility(height, width, visibility);
+
+        mOutQueue.add(String.valueOf(height));
+        mOutQueue.add(String.valueOf(width));
+        mOutQueue.add(String.valueOf(alarm));
+
+        mOutQueue.add(String.valueOf(mKY));
+        mOutQueue.add(String.valueOf(mKX));
+        putMaze(height, width, maze, visibility);
+
+        final Scanner scanner = PowerMockito.mock(Scanner.class);
+        PowerMockito.whenNew(Scanner.class).withAnyArguments().thenReturn(scanner);
+        PowerMockito.when(scanner.nextInt()).thenAnswer(new Answer<Integer>() {
+            @Override
+            public Integer answer(InvocationOnMock invocation) throws Throwable {
+                return Integer.parseInt(mOutQueue.pop());
+            }
+        });
+        PowerMockito.when(scanner.next()).thenAnswer(new Answer<String>() {
+            @Override
+            public String answer(InvocationOnMock invocation) throws Throwable {
+                return mOutQueue.pop();
+            }
+        });
+
+        PrintStream printStream = Mockito.mock(PrintStream.class);
+
+        Mockito.doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                final String answer = invocation.getArguments()[0].toString();
+                mOriginalOutPrintStream.println(answer);
+
+                if (answer.equals("UP")) {
+                    mKY--;
+                } else if (answer.equals("RIGHT")) {
+                    mKX++;
+                } else if (answer.equals("DOWN")) {
+                    mKY++;
+                } else if (answer.equals("LEFT")) {
+                    mKX--;
+                }
+
+                String currentCell = maze[mKY].substring(mKX, mKX + 1);
+
+                Assert.assertNotEquals("#", currentCell);
+
+                setVisibility(height, width, visibility);
+                mOutQueue.add(String.valueOf(mKY));
+                mOutQueue.add(String.valueOf(mKX));
+                putMaze(height, width, maze, visibility);
+                mTurnsLeft--;
+
+                if (mState == State.goToBack) {
+                    mTurnsBackLeft--;
+                }
+
+                if (mState == State.goToControl && currentCell.equals("C")) {
+                    mState = State.goToBack;
+                }
+
+                if (mState == State.goToBack && currentCell.equals("T")) {
+                    //finish
+                    throw new SuccessException();
+                }
+
+                Assert.assertTrue(mTurnsLeft > 0);
+                Assert.assertTrue(mTurnsBackLeft > 0);
+
+                return null;
+            }
+        }).when(printStream).println(Mockito.anyString());
+        System.setOut(printStream);
+
+        Player.main(null);
+    }
+
+    private void putMaze(int height, int width, String[] maze, boolean[][] visibility) {
+        StringBuilder row = new StringBuilder(width);
+        for (int y = 0; y < height; y++) {
+            row.setLength(0);
+            for (int x = 0; x < width; x++) {
+                boolean isVisible = visibility[x][y];
+                String symbol = isVisible ? maze[y].substring(x, x + 1) : "?";
+                if (isVisible && symbol.equals("?")) {
+                    Assert.fail("The maze is wrong");
+                }
+                row.append(symbol);
+            }
+            mOutQueue.add(row.toString());
+        }
+    }
+
+    private void setVisibility(int height, int width, boolean[][] visibility) {
+        for (int y = mKY - 2; y <= mKY + 2; y++) {
+            if (y >= 0 && y < height) {
+                for (int x = mKX - 2; x <= mKX + 2; x++) {
+                    if (x >= 0 && x < width) {
+                        visibility[x][y] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    private enum State {
+        goToControl, goToBack
+    }
+
+    private static class SuccessException extends Exception {
+
+    }
+}
