e4ba4038c78163775220eafb0e1d6d15a382e325
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2016-02-01 23:45:17 +0300

    paranoid android is better

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,8 +4,10 @@ class Player {
 
     private static Point sExitPoint;
     private static int sWidth;
+    private static int sAdditionalElevators;
     private static final HashSet<Point> sElevs = new HashSet<Point>();
     private static final HashSet<Point> sBlocked = new HashSet<Point>();
+    private static final HashSet<Integer> sFloorsWithoutElevators = new HashSet<Integer>();
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
@@ -15,15 +17,22 @@ class Player {
         int exitFloor = in.nextInt();
         int exitPos = in.nextInt();
         int nbTotalClones = in.nextInt(); // number of generated clones
-        int nbAdditionalElevators = in.nextInt(); // number of additional elevators
+        sAdditionalElevators = in.nextInt(); // number of additional elevators
         int nbElevators = in.nextInt(); // number of elevators
+
+        log("nbFloors=" + nbFloors + ", nbRounds=" + nbRounds + ", sAdditionalElevators=" + sAdditionalElevators +
+                ", nbTotalClones=" + nbTotalClones);
 
         sExitPoint = new Point(exitFloor, exitPos);
 
+        for (int i = 0; i < exitFloor; i++) {
+            sFloorsWithoutElevators.add(i);
+        }
         for (int i = 0; i < nbElevators; i++) {
             int elevatorFloor = in.nextInt(); // floor on which this elevator is found
             int elevatorPos = in.nextInt(); // position of the elevator on its floor
             sElevs.add(new Point(elevatorFloor, elevatorPos));
+            sFloorsWithoutElevators.remove(elevatorFloor);
         }
 
         final Point currentPoint = new Point(0, 0);
@@ -37,53 +46,94 @@ class Player {
             String action = "WAIT";
             currentPoint.floor = cloneFloor;
             currentPoint.pos = clonePos;
-            if (!direction.equals("NONE") && !sElevs.contains(currentPoint)) {
+            if (!direction.equals("NONE")) {
+                BestTurn bestTurn = getBestTurn(currentPoint);
                 final int directionOffset = direction.equals("LEFT") ? -1 : 1;
-
-                boolean isExitPresent = check(currentPoint, directionOffset);
-
-                if (!isExitPresent) {
-                    boolean isBackExist = check(currentPoint, -directionOffset);
-                    if (!isBackExist) {
+                switch (bestTurn) {
+                    case left:
+                        if (directionOffset > 0) {
+                            action = "BLOCK";
+                            sBlocked.add(new Point(cloneFloor, clonePos));
+                        }
+                        break;
+                    case right:
+                        if (directionOffset < 0) {
+                            action = "BLOCK";
+                            sBlocked.add(new Point(cloneFloor, clonePos));
+                        }
+                        break;
+                    case elevator:
                         action = "ELEVATOR";
                         sElevs.add(currentPoint);
-                        isExitPresent = true;
-                    }
-                }
-
-                if (!isExitPresent) {
-                    action = "BLOCK";
-                    sBlocked.add(new Point(cloneFloor, clonePos));
+                        sFloorsWithoutElevators.remove(cloneFloor);
+                        sAdditionalElevators--;
+                        break;
+                    case wait:
+                        break;
                 }
             }
 
-            // Write an action using System.out.println()
-            // To debug: System.err.println("Debug messages...");
-
             System.out.println(action); // action: WAIT or BLOCK or ELEVATOR
+            nbRounds--;
+            log(nbRounds + " rounds left");
         }
     }
 
-    /**
-     * Find the exit point.
-     * @param startPoint A start point
-     * @param direction Direction +-1 (right/left)
-     * @return The exit point or an elevator is present
-     */
-    private static boolean check(Point startPoint, int direction) {
+    private static BestTurn getBestTurn(Point currentPoint) {
+        if (sElevs.contains(currentPoint)) {
+            return BestTurn.wait;
+        }
+        CheckResult toRight = check(currentPoint, 1);
+        CheckResult toLeft = check(currentPoint, -1);
+        CheckResult toElevator;
+        if (sAdditionalElevators - sFloorsWithoutElevators.size() > 0) {
+            log("sae=" + sAdditionalElevators + ", ef=" + sExitPoint.floor + ", cf=" + currentPoint.floor + ", cfwe=" + sFloorsWithoutElevators.size());
+            toElevator = new CheckResult(false, true, 0);
+        } else {
+            toElevator = new CheckResult(false, false, 0);
+        }
+        log("r: " + toRight);
+        log("l: " + toLeft);
+        log("e: " + toElevator);
+
+        if (toRight.isExit) {
+            return BestTurn.right;
+        } else if (toLeft.isExit) {
+            return BestTurn.left;
+        } else if (toElevator.isPathExist || (!toRight.isPathExist && !toLeft.isPathExist)) {
+            return BestTurn.elevator;
+        } else if (toLeft.isPathExist && toRight.isPathExist) {
+            return toLeft.countTurnsToTheNextFloor > toRight.countTurnsToTheNextFloor ? BestTurn.right : BestTurn.left;
+        } else if (toLeft.isPathExist) {
+            return BestTurn.left;
+        } else {
+            return BestTurn.right;
+        }
+    }
+
+    private static void log(String message) {
+        System.err.println(message);
+    }
+
+    private static CheckResult check(Point startPoint, int direction) {
         final Point p = new Point(startPoint);
         p.pos = p.pos + direction;
+        int countTurns = 0;
         while (p.pos >= 0 && p.pos < sWidth) {
-            if (sExitPoint.equals(p) || sElevs.contains(p)) {
-                return true;
+            countTurns++;
+            if (sExitPoint.equals(p)) {
+                return new CheckResult(true, true, countTurns);
+            }
+            if (sElevs.contains(p)) {
+                return new CheckResult(false, true, countTurns);
             }
             if (sBlocked.contains(p)) {
-                return false;
+                break;
             }
             p.pos += direction;
         }
 
-        return false;
+        return new CheckResult(false, false, 0);
     }
 
     private static class Point {
@@ -109,6 +159,27 @@ class Player {
         @Override
         public int hashCode() {
             return floor * 1000 + pos;
+        }
+    }
+
+    private enum BestTurn {
+        wait, left, right, elevator
+    }
+
+    private static class CheckResult {
+        public final int countTurnsToTheNextFloor;
+        public final boolean isPathExist;
+        public final boolean isExit;
+
+        public CheckResult(boolean isExit, boolean isPathExist, int countTurnsToTheNextFloor) {
+            this.isExit = isExit;
+            this.isPathExist = isPathExist;
+            this.countTurnsToTheNextFloor = countTurnsToTheNextFloor;
+        }
+
+        @Override
+        public String toString() {
+            return "c=" + countTurnsToTheNextFloor + ", ipe=" + isPathExist + ", ie=" + isExit;
         }
     }
 }
