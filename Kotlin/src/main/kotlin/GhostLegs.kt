5aa9d4fa172b22921159906541f96f1e4a8f92b4
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2019-05-10 19:35:01 +0300

    Kotlin empty project was created

---
+++ Kotlin/src/main/kotlin/GhostLegs.kt
@@ -0,0 +1,31 @@
+import java.util.*
+import java.io.*
+import java.math.*
+
+/**
+ * https://www.codingame.com/ide/puzzle/ghost-legs
+ **/
+class GhostLegs {
+
+    companion object {
+        @JvmStatic
+        fun main(args : Array<String>) {
+/*
+            val input = Scanner(System.`in`)
+            val W = input.nextInt()
+            val H = input.nextInt()
+            if (input.hasNextLine()) {
+                input.nextLine()
+            }
+            for (i in 0 until H) {
+                val line = input.nextLine()
+            }
+*/
+
+            // Write an action using println()
+            // To debug: System.err.println("Debug messages...");
+
+            println("answer")
+        }
+    }
+}
