0f119af87f99cb59bbcaf4608db44456e2d298cf
Anton Kosov <kosov.anton.rodionovich@gmail.com>
2019-05-11 06:40:25 +0300

    GhostLegs

--- Kotlin/src/main/kotlin/GhostLegs.kt
+++ Kotlin/src/main/kotlin/GhostLegs.kt
@@ -1,31 +1,65 @@
+import java.lang.Exception
 import java.util.*
-import java.io.*
-import java.math.*
 
 /**
  * https://www.codingame.com/ide/puzzle/ghost-legs
  **/
+fun main() {
+    val input = Scanner(System.`in`)
+    input.nextInt() // width is not needed
+    val height = input.nextInt()
+    if (input.hasNextLine()) {
+        input.nextLine()
+    }
+    val ghostLegs = GhostLegs()
+    val firstLine = input.nextLine()
+    for (i in 0 until height - 2) {
+        val line = input.nextLine()
+        ghostLegs.addLine(line)
+        System.err.println(line)
+    }
+    val lastLine = input.nextLine()
+
+    println(ghostLegs.getAnswer(firstLine, lastLine))
+}
+
 class GhostLegs {
 
+/*
     companion object {
         @JvmStatic
-        fun main(args : Array<String>) {
-/*
-            val input = Scanner(System.`in`)
-            val W = input.nextInt()
-            val H = input.nextInt()
-            if (input.hasNextLine()) {
-                input.nextLine()
-            }
-            for (i in 0 until H) {
-                val line = input.nextLine()
-            }
+        fun main(args: Array<String>) {
+        }
+    }
 */
 
-            // Write an action using println()
-            // To debug: System.err.println("Debug messages...");
+    private var mCurrentState: IntArray? = null
 
-            println("answer")
+    fun addLine(line: String) {
+        if (mCurrentState == null)
+            mCurrentState = IntArray(1 + line.length / 3) { i -> i }
+
+        for (i in 0 until mCurrentState!!.size - 1) {
+            if (line[1 + i * 3] == ' ') continue
+            val t = mCurrentState!![i]
+            mCurrentState!![i] = mCurrentState!![i + 1]
+            mCurrentState!![i + 1] = t
         }
+    }
+
+    fun getAnswer(firstLine: String, lastLine: String): String {
+        if (mCurrentState == null)
+            throw Exception("No data.")
+
+        var result = ""
+
+        for (i in 0 until mCurrentState!!.size) {
+            if (result.isNotEmpty())
+                result += "\n"
+            val answerIndex = mCurrentState!!.indexOf(i)
+            result += firstLine[i * 3].toString() + lastLine[answerIndex * 3]
+        }
+
+        return result
     }
 }
