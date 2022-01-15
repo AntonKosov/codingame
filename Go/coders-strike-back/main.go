b5322bae79d2969405c3682d0c9e7d1a4c4859cb
Anton Kosov <AntonRKosov@gmail.com>
2022-01-14 19:43:59 -0600

    coders-strike-back - promoted to bronze league

--- Go/coders-strike-back/main.go
+++ Go/coders-strike-back/main.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	boostUsed := false
 	for {
 		// nextCheckpointX: x position of the next check point
 		// nextCheckpointY: y position of the next check point
@@ -21,8 +22,14 @@ func main() {
 		// You have to output the target position
 		// followed by the power (0 <= thrust <= 100)
 		// i.e.: "x y thrust"
-		trust := trustMin
 		nextCheckpointAngle = abs(nextCheckpointAngle)
+		if !boostUsed && nextCheckpointAngle < 15 && nextCheckpointDist > mapWidth/3 {
+			fmt.Printf("%d %d %s\n", nextCheckpointX, nextCheckpointY, boostAction)
+			boostUsed = true
+			continue
+		}
+
+		trust := trustMin
 		if nextCheckpointAngle < 90 {
 			trust = trustMax - trustMax*(abs(nextCheckpointAngle)/90)
 		}
@@ -34,7 +41,10 @@ const (
 	trustMin = 0
 	trustMax = 100
 
-// mapWidth  = 1600
+	boostAction = "BOOST"
+
+	mapWidth = 1600
+
 // mapHeight = 900
 // checkpointRadius = 600
 // firstTurnTimeout = time.Millisecond * 1000
