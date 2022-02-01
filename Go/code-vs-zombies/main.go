4968c13e71a655d99a9ae8bf69ed8e8121c3fef1
Anton Kosov <AntonRKosov@gmail.com>
2022-02-01 04:48:50 -0600

    code-vs-zombies

--- Go/code-vs-zombies/main.go
+++ Go/code-vs-zombies/main.go
@@ -13,6 +13,7 @@ const Debug = false
 // const Debug = true
 
 // TODOs:
+// * The simulation is slightly wrong!
 // * Consider only those states where maximum humans are alive?
 // * [Test: Cross] Implement the detection of loosing all humans in the future
 // * [Low priority] Hunt zombies even if there is no chance to win
@@ -24,13 +25,13 @@ const Debug = false
 // * [Performance] Get rid of copying maps
 func main() {
 	ctx := context.Background()
-	var expectedState *State
+	var nextState *State
 	for {
-		expectedState = process(ctx, expectedState)
+		nextState = process(ctx, nextState)
 	}
 }
 
-func process(ctx context.Context, expectedState *State) *State {
+func process(ctx context.Context, nextState *State) *State {
 	var x, y int
 	fmt.Scan(&x, &y)
 	player := NewVector(x, y)
@@ -53,17 +54,19 @@ func process(ctx context.Context, expectedState *State) *State {
 		zombies[NewVector(zombieX, zombieY)]++
 	}
 
-	fmt.Fprintf(os.Stderr, "Player: %+v; humans: %v; zombies: %v\n", player, len(humans), len(zombies))
-	state := NewState(player, humans, zombies)
-
-	if Debug && expectedState != nil {
-		state.ValidateWithExpectedState(expectedState)
+	// fmt.Fprintf(os.Stderr, "Player: %+v; humans: %v; zombies: %v\n", player, len(humans), len(zombies))
+	currentState := nextState
+	if currentState == nil {
+		currentState = NewState(player, humans, zombies)
+	} else if Debug {
+		actualState := NewState(player, humans, zombies)
+		actualState.ValidateWithExpectedState(currentState)
 	}
 
 	ctx, cancel := context.WithTimeout(ctx, Timeout)
 	defer cancel()
 
-	nextState := findOptimalDestination(ctx, state)
+	nextState = findNextState(ctx, currentState)
 	destination := nextState.player //nextState.player.Sub(state.player)
 
 	// fmt.Fprintln(os.Stderr, "Debug messages...")
@@ -84,92 +87,58 @@ const (
 	FieldHeight      = 9000
 )
 
-type Vector struct {
-	X, Y int
+type NextStates struct {
+	states         map[Vector]*State // player's position -> State
+	bestChildState *State
+	isWinner       bool
 }
 
-func NewVector(x, y int) Vector {
-	return Vector{X: x, Y: y}
-}
-
-func (v Vector) Add(av Vector) Vector {
-	return Vector{X: v.X + av.X, Y: v.Y + av.Y}
-}
-
-func (v Vector) Sub(av Vector) Vector {
-	return Vector{X: v.X - av.X, Y: v.Y - av.Y}
-}
-
-func (v Vector) Scale(f float64) Vector {
-	const eps = 0.00000000001
-	ex := eps
-	if v.X < 0 {
-		ex *= -1
+func (ns *NextStates) saveBestChildState(state *State) {
+	if ns.bestChildState == nil {
+		ns.bestChildState = state
 	}
-	ey := eps
-	if v.Y < 0 {
-		ey *= -1
+	if ns.bestChildState.Compare(state) >= 0 {
+		return
 	}
-	x := float64(v.X)*f + ex
-	y := float64(v.Y)*f + ey
-	return NewVector(int(x), int(y))
-}
-
-func (v Vector) Len() int {
-	return Sqrt(v.Len2())
-}
-
-func (v Vector) Len2() int {
-	return v.X*v.X + v.Y*v.Y
-}
-
-func moveTo(start, destination Vector, maxDistance int) Vector {
-	offset := destination.Sub(start)
-	if offset.Len2() <= maxDistance*maxDistance {
-		return destination
+	ns.bestChildState = state
+	for pos, s := range ns.states {
+		if s.MaxPossibleScore() < state.score {
+			delete(ns.states, pos)
+		}
 	}
-
-	f := float64(maxDistance) / math.Sqrt(float64(offset.Len2()))
-	offset = offset.Scale(f)
-
-	return start.Add(offset)
-}
-
-type Humans map[Vector]struct{}
-
-func (vl Humans) Clone() Humans {
-	clone := make(Humans, len(vl))
-	for v := range vl {
-		clone[v] = struct{}{}
+	ns.isWinner = state.IsWinner() && len(ns.states) == 1
+	if state.parent != nil {
+		state.parent.nextStates.saveBestChildState(state)
 	}
-	return clone
 }
 
-type Zombies map[Vector]int
-
-func (z Zombies) Clone() Zombies {
-	clone := make(Zombies, len(z))
-	for z, c := range z {
-		clone[z] = c
+func (ns *NextStates) AddState(state *State) {
+	if ns.bestChildState == nil || state.MaxPossibleScore() >= ns.bestChildState.score {
+		ns.states[state.player] = state
+		ns.saveBestChildState(state)
 	}
-	return clone
 }
 
-var bonusScoreMul []int
+func (ns *NextStates) IsAnalyzed() bool {
+	return ns.bestChildState != nil
+}
 
-func init() {
-	fib := make([]int, MaxZombies+1)
-	fib[1] = 1
-	fib[2] = 2
-	for i := 3; i <= MaxZombies; i++ {
-		fib[i] = fib[i-2] + fib[i-1]
+func (ns *NextStates) PickStateToAnalyze() *State {
+	var chosenState *State
+	for _, s := range ns.states {
+		if !s.nextStates.IsAnalyzed() {
+			return s
+		}
+		if s.nextStates.isWinner {
+			continue
+		}
+		if chosenState == nil || chosenState.Compare(s) < 0 {
+			s := s
+			chosenState = s
+		}
 	}
 
-	bonusScoreMul = make([]int, MaxZombies+1)
-	bonusScoreMul[1] = 1
-	for i := 2; i <= MaxZombies; i++ {
-		bonusScoreMul[i] = bonusScoreMul[i-1] + fib[i]
-	}
+	return chosenState
 }
 
 type State struct {
@@ -177,36 +146,72 @@ type State struct {
 	humans  Humans
 	zombies Zombies
 
-	firstState       *State
-	bestMovements    []Vector
-	score            int
-	turns            int
-	humansMaySurvive int
+	parent *State
+
+	nextStates         NextStates
+	turns              int
+	score              int
+	maxAdditionalScore int
 }
 
-func NewState(player Vector, humans Humans, zombies Zombies) State {
-	ns := State{
+func NewState(player Vector, humans Humans, zombies Zombies) *State {
+	return &State{
 		player:  player,
 		humans:  humans,
 		zombies: zombies,
+		nextStates: NextStates{
+			states: make(map[Vector]*State),
+		},
 	}
-	ns.prepareMovements()
-	return ns
 }
 
-func (s *State) Clone() State {
-	return State{
-		player:  s.player,
-		humans:  s.humans.Clone(),
-		zombies: s.zombies.Clone(),
+func (s *State) IsWinner() bool {
+	return len(s.zombies) == 0
+}
 
-		firstState: s.firstState,
-		score:      s.score,
-		turns:      s.turns,
+func (s *State) MaxPossibleScore() int {
+	return s.score + s.maxAdditionalScore
+}
+
+func (s *State) Compare(as *State) int {
+	maxPossibleScoreDiff := s.MaxPossibleScore() - as.MaxPossibleScore()
+	if maxPossibleScoreDiff != 0 {
+		return maxPossibleScoreDiff
 	}
+	scoreDiff := s.score - as.score
+	if scoreDiff != 0 {
+		return scoreDiff
+	}
+	turnsDiff := s.turns - as.turns
+	if turnsDiff != 0 {
+		return turnsDiff
+	}
+	w := 0
+	if s.IsWinner() {
+		w++
+	}
+	if as.IsWinner() {
+		w--
+	}
+	return w
+}
+
+func (s *State) Clone() *State {
+	cs := NewState(s.player, s.humans.Clone(), s.zombies.Clone())
+	cs.score = s.score
+	return cs
+}
+
+func (s *State) AddNextState(ns *State) {
+	ns.turns = s.turns + 1
+	ns.parent = s
+	s.nextStates.AddState(ns)
 }
 
 func (s *State) moveZombies() {
+	//TODO Zombies should cache their targets and check before moving:
+	// * if the player is closer
+	// * if the human is still there
 	moved := make(Zombies, len(s.zombies))
 	for z := range s.zombies {
 		closest := s.player
@@ -224,11 +229,11 @@ func (s *State) moveZombies() {
 	s.zombies = moved
 }
 
-func (s *State) safetyDistanceAfterZombiesMove2() map[Vector]int {
-	safety := make(map[Vector]int, len(s.humans))
-	for h := range s.humans {
+func safetyDistanceAfterZombiesMove2(state *State) map[Vector]int {
+	safety := make(map[Vector]int, len(state.humans))
+	for h := range state.humans {
 		minDist2 := FieldWidth * FieldWidth
-		for z := range s.zombies {
+		for z := range state.zombies {
 			d2 := z.Sub(h).Len2()
 			minDist2 = Min(minDist2, d2)
 		}
@@ -264,11 +269,7 @@ func (s *State) destroyZombies() int {
 		delete(s.zombies, dz)
 	}
 
-	return earnedScore(len(s.humans), count)
-}
-
-func earnedScore(humansLeft, zombiesKilled int) int {
-	return humansLeft * humansLeft * 10 * bonusScoreMul[zombiesKilled]
+	return Score(len(s.humans), count)
 }
 
 func (s *State) killHumans() {
@@ -277,177 +278,103 @@ func (s *State) killHumans() {
 	}
 }
 
-func findOptimalDestination(ctx context.Context, state State) *State {
-	states := []State{state}
-	var winState *State
-	totalVariants := 0
-	depth := 0
-timeout:
-	for len(states) > 0 {
-		nextStates := make([]State, 0, len(states))
-		depth++
-		for _, s := range states {
-			select {
-			case <-ctx.Done():
-				fmt.Fprintln(os.Stderr, "Timeout")
-				states = append(states, nextStates...)
-				break timeout
-			default:
-				newStates := tryVariants(ctx, s)
-				for _, s := range newStates {
-					s := s
-					if len(s.zombies) == 0 {
-						if winState == nil || (winState.score < s.score ||
-							(winState.score == s.score && winState.turns > s.turns)) {
-							winState = &s
-						}
-					} else {
-						nextStates = append(nextStates, s)
-					}
-				}
-				totalVariants += len(newStates)
-			}
+func (s *State) EstimateBestState() bool {
+	for s.nextStates.IsAnalyzed() {
+		s = s.nextStates.PickStateToAnalyze()
+		if s == nil {
+			return false
 		}
-		states = nextStates
 	}
+	s.Estimate()
+	return true
+}
 
-	if winState != nil {
-		states = append(states, *winState)
+func (s *State) DetachBestMovement() *State {
+	bc := s.nextStates.bestChildState
+	b := bc
+	for b.parent != s {
+		b = b.parent
 	}
-	var bestState *State
-	for _, s := range states {
-		s := s
-		hms := s.humansMaySurvive
-		if bestState != nil && bestState.humansMaySurvive > hms {
-			continue
-		} else if bestState != nil && bestState.score > s.score {
-			continue
-		}
-		bestState = &s
-	}
-	fmt.Fprintf(os.Stderr, "Total variants: %v; Non winners: %v; Depth: %v; Survivors: %v, Winner: %v\n",
-		totalVariants, len(states), depth, bestState.humansMaySurvive, winState != nil,
+	b.parent = nil
+	fmt.Fprintf(os.Stderr, "Best child: turns=%v, score=%v, max_pos_score=%v, winner=%v\n",
+		bc.turns, bc.score, bc.MaxPossibleScore(), bc.IsWinner(),
 	)
-	// fmt.Fprintf(os.Stderr, "Player: %+v, zombies: %v\n",
-	// 	state.player, len(bestState.zombies),
-	// )
-
-	return bestState.firstState
+	return b
 }
 
-const movementAngle = 15
+// TODO add context?
+func (s *State) Estimate() {
+	nextMovements := prepareMovements(s)
+	s.moveZombies()
 
-var movements []Vector
+	for pos, survivors := range nextMovements {
+		nextState := s.Clone()
+		nextState.movePlayer(pos)
+		nextState.score += nextState.destroyZombies()
+		nextState.killHumans()
+		nextState.maxAdditionalScore = Score(survivors, len(nextState.zombies))
+		s.AddNextState(nextState)
+	}
+}
 
-func init() {
-	movements = make([]Vector, 0, 360/movementAngle+360/movementAngle/2+1)
-	zero := NewVector(0, 0)
-	movements = append(movements, zero)
-	for i := 0; i*movementAngle < 360; i++ {
-		angleRad := float64(i*movementAngle) * math.Pi / 180.0
-		s := math.Sin(angleRad)
-		c := math.Cos(angleRad)
-		dir := NewVector(int(c*PlayerMaxStep*10), int(s*PlayerMaxStep*10))
-		movements = append(movements, moveTo(zero, dir, PlayerMaxStep))
-		if i%2 == 0 {
-			movements = append(movements, moveTo(zero, dir, ZombieStep))
+func findNextState(ctx context.Context, state *State) *State {
+	estimations := 0
+stop:
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Fprintln(os.Stderr, "Timeout")
+			break stop
+		default:
+			if !state.EstimateBestState() {
+				fmt.Fprintln(os.Stderr, "No need to analyze")
+				break stop
+			}
+			estimations++
 		}
 	}
+
+	fmt.Fprintf(os.Stderr, "Estimations: %v\n", estimations)
+	return state.DetachBestMovement()
 }
 
-func (s *State) possibleMovementsTowardCharacters() map[Vector]struct{} {
-	movements := make(map[Vector]struct{}, len(s.humans)+len(s.zombies)+1)
-	movements[s.player] = struct{}{}
-	for h := range s.humans {
-		np := moveTo(s.player, h, PlayerMaxStep)
-		movements[np] = struct{}{}
-	}
-	for z := range s.zombies {
-		np := moveTo(s.player, z, PlayerMaxStep)
-		// zombie won't be at this place next turn
-		movements[np] = struct{}{}
-	}
-	return movements
-}
-
-func (s *State) possibleMovementsAround() map[Vector]struct{} {
-	// possibleMovements := make(map[Vector]struct{}, len(movements)+1)
-	possibleMovements := make(map[Vector]struct{}, len(s.humans)+len(movements)+1)
+func possibleMovementsAround(s *State) map[Vector]struct{} {
+	possibleMovements := make(map[Vector]struct{}, len(movements)+1)
+	// possibleMovements := make(map[Vector]struct{}, len(s.humans)+len(movements)+1)
 	possibleMovements[s.player] = struct{}{}
-	for h := range s.humans {
-		np := moveTo(s.player, h, PlayerMaxStep)
-		possibleMovements[np] = struct{}{}
+	add := func(pos Vector) {
+		if pos.X < 0 || pos.Y < 0 || pos.X >= FieldWidth || pos.Y >= FieldHeight {
+			return
+		}
+		possibleMovements[pos] = struct{}{}
 	}
+	// for h := range s.humans {
+	// 	np := moveTo(s.player, h, PlayerMaxStep)
+	// 	add(np)
+	// }
 	for _, move := range movements {
-		possibleMovements[s.player.Add(move)] = struct{}{}
+		add(s.player.Add(move))
 	}
 	return possibleMovements
 }
 
-func (s *State) prepareMovements() {
-	safetyDistance2 := s.safetyDistanceAfterZombiesMove2()
-	draft := make(map[Vector]int, len(movements)+len(safetyDistance2))
-	maxSafetyPoints := 0
-	survivedHumans := make(map[Vector]struct{}, len(s.humans))
-	addPoint := func(pos Vector) {
-		if pos.X < 0 || pos.Y < 0 || pos.X >= FieldWidth || pos.Y >= FieldHeight {
-			return
-		}
+func prepareMovements(state *State) map[Vector]int { // Position -> the number of survivors
+	safetyDistance2 := safetyDistanceAfterZombiesMove2(state)
+	possibleMoves := possibleMovementsAround(state)
+	nextMovements := make(map[Vector]int, len(possibleMoves))
+	for pos := range possibleMoves {
+		maySurvive := 0
 		for hPos, sd2 := range safetyDistance2 {
 			d2 := pos.Sub(hPos).Len2()
-			if d2 > sd2 {
-				continue
+			if d2 <= sd2 {
+				maySurvive++
 			}
-			points := draft[pos] + 1
-			draft[pos] = points
-			maxSafetyPoints = Max(maxSafetyPoints, points)
-			survivedHumans[hPos] = struct{}{}
+		}
+		if maySurvive > 0 {
+			nextMovements[pos] = maySurvive
 		}
 	}
-	// possibleMoves := s.possibleMovementsTowardCharacters()
-	possibleMoves := s.possibleMovementsAround()
-	for p := range possibleMoves {
-		addPoint(p)
-	}
-
-	moves := make([]Vector, 0, len(draft))
-	for p, points := range draft {
-		if points == maxSafetyPoints {
-			moves = append(moves, p)
-		}
-	}
-
-	s.humansMaySurvive = len(survivedHumans)
-
-	s.bestMovements = moves
-}
-
-func tryVariants(ctx context.Context, state State) []State {
-	nextStates := make([]State, 0, len(state.bestMovements))
-	state.moveZombies()
-
-	for _, destination := range state.bestMovements {
-		select {
-		case <-ctx.Done():
-			return nextStates
-		default:
-			stateClone := state.Clone()
-			stateClone.movePlayer(destination)
-			stateClone.score += stateClone.destroyZombies()
-			stateClone.turns++
-			stateClone.killHumans()
-			stateClone.prepareMovements()
-			if stateClone.humansMaySurvive == 0 {
-				continue
-			}
-			if stateClone.firstState == nil {
-				stateClone.firstState = &stateClone
-			}
-			nextStates = append(nextStates, stateClone)
-		}
-	}
-
-	return nextStates
+	return nextMovements
 }
 
 func Min(a, b int) int {
@@ -492,6 +419,118 @@ func (s *State) ValidateWithExpectedState(e *State) {
 			panic(fmt.Sprintf("Zombie not found at %+v, actual zombies: %+v, expected zombies: %+v",
 				z, s.zombies, e.zombies,
 			))
+		}
+	}
+}
+
+type Vector struct {
+	X, Y int
+}
+
+func NewVector(x, y int) Vector {
+	return Vector{X: x, Y: y}
+}
+
+func (v Vector) Add(av Vector) Vector {
+	return Vector{X: v.X + av.X, Y: v.Y + av.Y}
+}
+
+func (v Vector) Sub(av Vector) Vector {
+	return Vector{X: v.X - av.X, Y: v.Y - av.Y}
+}
+
+func (v Vector) Scale(f float64) Vector {
+	const eps = 0.00000000001
+	ex := eps
+	if v.X < 0 {
+		ex *= -1
+	}
+	ey := eps
+	if v.Y < 0 {
+		ey *= -1
+	}
+	x := float64(v.X)*f + ex
+	y := float64(v.Y)*f + ey
+	return NewVector(int(x), int(y))
+}
+
+func (v Vector) Len() int {
+	return Sqrt(v.Len2())
+}
+
+func (v Vector) Len2() int {
+	return v.X*v.X + v.Y*v.Y
+}
+
+type Humans map[Vector]struct{}
+
+func (vl Humans) Clone() Humans {
+	clone := make(Humans, len(vl))
+	for v := range vl {
+		clone[v] = struct{}{}
+	}
+	return clone
+}
+
+type Zombies map[Vector]int
+
+func (z Zombies) Clone() Zombies {
+	clone := make(Zombies, len(z))
+	for z, c := range z {
+		clone[z] = c
+	}
+	return clone
+}
+
+var bonusScoreMul []int
+
+func init() {
+	fib := make([]int, MaxZombies+1)
+	fib[1] = 1
+	fib[2] = 2
+	for i := 3; i <= MaxZombies; i++ {
+		fib[i] = fib[i-2] + fib[i-1]
+	}
+
+	bonusScoreMul = make([]int, MaxZombies+1)
+	bonusScoreMul[1] = 1
+	for i := 2; i <= MaxZombies; i++ {
+		bonusScoreMul[i] = bonusScoreMul[i-1] + fib[i]
+	}
+}
+
+func Score(humansLeft, zombiesKilled int) int {
+	return humansLeft * humansLeft * 10 * bonusScoreMul[zombiesKilled]
+}
+
+func moveTo(start, destination Vector, maxDistance int) Vector {
+	offset := destination.Sub(start)
+	if offset.Len2() <= maxDistance*maxDistance {
+		return destination
+	}
+
+	f := float64(maxDistance) / math.Sqrt(float64(offset.Len2()))
+	offset = offset.Scale(f)
+
+	return start.Add(offset)
+}
+
+const movementAngle = 15
+
+var movements []Vector
+
+func init() {
+	movements = make([]Vector, 0, 360/movementAngle+360/movementAngle/2+1)
+	zero := NewVector(0, 0)
+	movements = append(movements, zero)
+	for i := 0; i*movementAngle < 360; i++ {
+		angleRad := float64(i*movementAngle) * math.Pi / 180.0
+		s := math.Sin(angleRad)
+		c := math.Cos(angleRad)
+		dir := NewVector(int(c*PlayerMaxStep*10), int(s*PlayerMaxStep*10))
+		movements = append(movements, moveTo(zero, dir, PlayerMaxStep))
+		if i%2 == 0 {
+			movements = append(movements, moveTo(zero, dir, ZombieStep))
 		}
 	}
 }
--- Go/code-vs-zombies/main_test.go
+++ Go/code-vs-zombies/main_test.go
@@ -7,20 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func runEmulation(t *testing.T, startState State, survivors, minScore int) {
+func runSimulation(t *testing.T, startState *State, survivors, minScore int) {
 	state := startState
-	score := 0
 	for len(state.zombies) > 0 {
 		ctx, cancel := context.WithTimeout(context.Background(), Timeout)
-		nextState := findOptimalDestination(ctx, state)
-		score += nextState.score
+		state = findNextState(ctx, state)
 		require.True(t, len(state.humans) > 0)
-		state = NewState(nextState.player, nextState.humans, nextState.zombies)
 		cancel()
 	}
 
 	require.Equal(t, survivors, len(state.humans))
-	require.GreaterOrEqual(t, score, minScore)
+	require.GreaterOrEqual(t, state.score, minScore)
 }
 
 func TestMoveTo1(t *testing.T) {
@@ -44,62 +41,62 @@ func TestMoveTo3(t *testing.T) {
 }
 
 func TestEarningScoreZeroZombies(t *testing.T) {
-	require.Equal(t, 0, earnedScore(1, 0))
+	require.Equal(t, 0, Score(1, 0))
 }
 
 func TestEarningScoreOneZombieOneHuman(t *testing.T) {
-	require.Equal(t, 10, earnedScore(1, 1))
+	require.Equal(t, 10, Score(1, 1))
 }
 
 func TestEarningScoreTwoZombieOneHuman(t *testing.T) {
-	require.Equal(t, 30, earnedScore(1, 2))
+	require.Equal(t, 30, Score(1, 2))
 }
 
-func TestShouldSurviveAfterOneStep(t *testing.T) {
-	player := NewVector(0, 0)
-	humans := Humans{NewVector(3000, 0): struct{}{}}
-	zombies := Zombies{NewVector(3000+ZombieStep/2, 0): 1}
-	state := NewState(player, humans, zombies)
-	state.prepareMovements()
-	require.Equal(t, 1, state.humansMaySurvive)
-}
+// func TestShouldSurviveAfterOneStep(t *testing.T) {
+// 	player := NewVector(0, 0)
+// 	humans := Humans{NewVector(3000, 0): struct{}{}}
+// 	zombies := Zombies{NewVector(3000+ZombieStep/2, 0): 1}
+// 	state := NewState(player, humans, zombies)
+// 	state.prepareMovements()
+// 	require.Equal(t, 1, state.humansMaySurvive)
+// }
+//
+// func TestShouldSurviveInTest1(t *testing.T) {
+// 	player := NewVector(0, 0)
+// 	humans := Humans{NewVector(8250, 4500): struct{}{}}
+// 	zombies := Zombies{NewVector(8250, 8999): 1}
+// 	state := NewState(player, humans, zombies)
+// 	state.prepareMovements()
+// 	require.Equal(t, 1, state.humansMaySurvive)
+// }
+//
+// func TestShouldSurviveAfterThreeSteps(t *testing.T) {
+// 	player := NewVector(0, 0)
+// 	humans := Humans{NewVector(5000, 0): struct{}{}}
+// 	zombies := Zombies{NewVector(5000-3*ZombieStep, 1): 1}
+// 	state := NewState(player, humans, zombies)
+// 	state.prepareMovements()
+// 	require.Equal(t, 1, state.humansMaySurvive)
+// }
+//
+// func TestShouldNotSurviveAfterThreeSteps(t *testing.T) {
+// 	player := NewVector(0, 0)
+// 	humans := Humans{NewVector(5000, 0): struct{}{}}
+// 	zombies := Zombies{NewVector(5000, 2*ZombieStep-ZombieStep/2): 1}
+// 	state := NewState(player, humans, zombies)
+// 	state.prepareMovements()
+// 	require.Equal(t, 0, state.humansMaySurvive)
+// }
 
-func TestShouldSurviveInTest1(t *testing.T) {
-	player := NewVector(0, 0)
+func TestCase1(t *testing.T) {
 	humans := Humans{NewVector(8250, 4500): struct{}{}}
 	zombies := Zombies{NewVector(8250, 8999): 1}
-	state := NewState(player, humans, zombies)
-	state.prepareMovements()
-	require.Equal(t, 1, state.humansMaySurvive)
-}
-
-func TestShouldSurviveAfterThreeSteps(t *testing.T) {
-	player := NewVector(0, 0)
-	humans := Humans{NewVector(5000, 0): struct{}{}}
-	zombies := Zombies{NewVector(5000-3*ZombieStep, 1): 1}
-	state := NewState(player, humans, zombies)
-	state.prepareMovements()
-	require.Equal(t, 1, state.humansMaySurvive)
-}
-
-func TestShouldNotSurviveAfterThreeSteps(t *testing.T) {
-	player := NewVector(0, 0)
-	humans := Humans{NewVector(5000, 0): struct{}{}}
-	zombies := Zombies{NewVector(5000, 2*ZombieStep-ZombieStep/2): 1}
-	state := NewState(player, humans, zombies)
-	state.prepareMovements()
-	require.Equal(t, 0, state.humansMaySurvive)
-}
-
-func TestEmulateTest1(t *testing.T) {
-	humans := Humans{NewVector(8250, 4500): struct{}{}}
-	zombies := Zombies{NewVector(8250, 8999): 1}
 	player := NewVector(0, 0)
 	state := NewState(player, humans, zombies)
-	runEmulation(t, state, 1, 10)
+	runSimulation(t, state, 1, 10)
 }
 
-func TestEmulateTest2(t *testing.T) {
+func TestCase2(t *testing.T) {
 	humans := Humans{
 		NewVector(950, 6000):  struct{}{},
 		NewVector(8000, 6100): struct{}{},
@@ -110,10 +107,10 @@ func TestEmulateTest2(t *testing.T) {
 	}
 	player := NewVector(0, 0)
 	state := NewState(player, humans, zombies)
-	runEmulation(t, state, 2, 80)
+	runSimulation(t, state, 2, 80)
 }
 
-func TestEmulateTest4(t *testing.T) {
+func TestCase4(t *testing.T) {
 	humans := Humans{NewVector(8000, 4500): struct{}{}}
 	zombies := Zombies{
 		NewVector(2000, 6500):  1,
@@ -121,5 +118,5 @@ func TestEmulateTest4(t *testing.T) {
 	}
 	player := NewVector(8000, 2000)
 	state := NewState(player, humans, zombies)
-	runEmulation(t, state, 1, 30)
+	runSimulation(t, state, 1, 30)
 }
